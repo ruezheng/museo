b4ef826a4cd44b4872bd356264acaaed4f5d8deb
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-18 21:16:55 -0600

    Fix: classes

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,2 +1,14 @@
 
 # For `age_at_death`, assume an artist was born and died on the same day.
+
+class Artist
+  attr_reader :id, :name, :born, :died, :country
+
+  def initialize(attributes)
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @born = attributes[:born]
+    @died = attributes[:died]
+    @country = attributes[:country]
+  end
+end
